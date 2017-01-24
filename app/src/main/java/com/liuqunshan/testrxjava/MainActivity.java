846ecf803e75971cb0818cc5306b354f4cfd3e55
liuqunshan <liuqunshan@hisense.com>
2017-01-24 17:59:20 +0800

    测试RxJava的部分operator和scheduler。

--- app/src/main/java/com/liuqunshan/testrxjava/MainActivity.java
+++ app/src/main/java/com/liuqunshan/testrxjava/MainActivity.java
@@ -16,15 +16,22 @@ import rx.Observable;
 import rx.Observer;
 import rx.Producer;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
+import rx.observables.AsyncOnSubscribe;
 import rx.observers.Subscribers;
+import rx.plugins.RxJavaHooks;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Subscription subscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +54,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        testBase();
+//        testBase();
+        testOperatorAndScheduler();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
     }
 
     // 测试一些基本用法
@@ -102,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         // 同一个Observer可以subscribe多次，每次都将调用其onNext()和onCompleted()回调
-        Observable.from(new String[]{null, "a1", "b1", "c1", ""}).subscribe(observer);
+        Observable.from(new String[]{"a1", "b1", "c1"}).subscribe(observer);
         Observable.just("a2", "b2", "c2").subscribe(observer);
 
         // FIXME: 同一个Subscriber只能subscribe一次，
@@ -115,6 +132,109 @@ public class MainActivity extends AppCompatActivity {
         Observable.from(new String[]{"d2", "e2", "f2"}).subscribe(subscriber);
 
         Log.w(TAG, "subscribe: " + Thread.currentThread());
+    }
+
+    // 测试各种各样的Operator和scheduler线程切换
+    private void testOperatorAndScheduler() {
+        Subscriber<Object> subscriber = new Subscriber<Object>() {
+
+            @Override
+            public void setProducer(Producer p) {
+                super.setProducer(p);
+                Log.i(TAG, "subscriber @@@@ setProducer: " + p + " " + Thread.currentThread());
+            }
+
+            @Override
+            public void onStart() {
+                super.onStart();
+                Log.i(TAG, "subscriber @@@@ onStart: " + Thread.currentThread());
+            }
+
+            @Override
+            public void onCompleted() {
+                Log.i(TAG, "subscriber @@@@ onCompleted: " + Thread.currentThread());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                Log.i(TAG, "subscriber @@@@ onError: " + throwable + " " + Thread.currentThread());
+            }
+
+            @Override
+            public void onNext(Object s) {
+                Log.i(TAG, "subscriber @@@@ onNext: " + s + " " + Thread.currentThread());
+            }
+        };
+
+        // FIXME: subscribeOn()可以调用多次，会影响到之前的lift operation call线程
+        // FIXME: observeOn()可以调用多次，会影响到之前的lift operation callback线程，进而影响到之后的lift operation call线程
+        // FIXME: 一个operator上边用subscribeOn()指定了call线程，下边用observeOn()指定了callback线程时，以上边的subscribeOn()线程优先
+        // FIXME: 一般情况下，只需要在整个Observable链式调用的末尾subscribe之前各调用一次subscribeOn()和observeOn()就可以了
+        // FIXME: delay等部分operator会切换线程
+
+        // 每隔1s产生一个Observable<Integer>
+//        subscription = Observable.interval(1, TimeUnit.SECONDS)
+        // 连续产生3个Observable
+        subscription =  Observable.range(1, 3)
+
+                // 展开成多个Observable并映射成另外多个Observable
+                .flatMap(new Func1<Object, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(Object s) {
+                        Log.w(TAG, "flat map Observable.call(): " + s + " " + Thread.currentThread());
+                        return Observable.from(new String[]{s + "_new1"/*, "_new2_" + s*/});
+                    }
+                })
+
+                // FIXME: 上边的flatMap操作call将发生在新线程
+                // Observable call方法（事件生产）线程，创建新线程
+                .subscribeOn(Schedulers.newThread())
+
+                // FIXME: 设置上边的flatMap操作callback将发生在Computation线程，因此下边的filter操作call都将发生在Computation线程
+                // Observer回调方法（事件消费）线程，使用Computation线程
+                .observeOn(Schedulers.computation())
+
+                // 过滤部分Observable，返回false时被过滤掉
+                .filter(new Func1<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) {
+                        Log.w(TAG, "filter Observable.call(): " + s + " " + Thread.currentThread());
+                        return s != null;
+                    }
+                })
+
+                // FIXME: 设置上边的filter call发生在IO线程，但是上边的observeOn()已经指定了Computation线程，所以此处指定io线程不成功
+                // Observable call方法（事件生产）线程，使用IO线程
+                .subscribeOn(Schedulers.io())
+
+                // FIXME: 设置上边的filter callback发生在Main线程，所以下边的map call发生在Main线程
+                // Observer回调方法（事件消费）线程，使用MAIN线程
+                .observeOn(AndroidSchedulers.from(getMainLooper()))
+
+                // 映射Observable<String> => Observable<Integer>
+                .map(new Func1<String, Integer>() {
+                    @Override
+                    public Integer call(String s) {
+                        Log.w(TAG, "map Observable.call(): " + s + " "
+                                + (int) s.charAt(0) + " " + Thread.currentThread());
+                        return (int) s.charAt(0);
+                    }
+                })
+
+                // FIXME: delay调用将导致上边的map callback发生在Computation线程，所以最终的subscriber callback也将转移到Computation线程
+                // Observable call方法调用之后延时1s再到observer回调方法，默认是Computation线程
+                .delay(1, TimeUnit.SECONDS/*, AndroidSchedulers.mainThread()*/)
+
+                // 在subscriber的onStart()回调之后同线程调用，可以用来做界面初始化之类的工作
+                .doOnSubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        Log.w(TAG, "@@@@@@@@ do on subscribe: " + Thread.currentThread());
+                    }
+                })
+
+                // 达成订阅
+                .subscribe(subscriber);
     }
 
     @Override
